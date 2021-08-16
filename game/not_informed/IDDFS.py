7a964f28bde3d2d5ee9a709dbdbb3e02d042483b
nicbritos <nicbritos@hotmail.com>
2021-08-15 23:41:05 -0300

    Fix IDDFS

--- game/not_informed/IDDFS.py
+++ game/not_informed/IDDFS.py
@@ -24,6 +24,7 @@ class IDDFS:
             print('Trying with', max_depth)
 
     def _process_with_depth(self, max_depth):
+        found_state = None
         while len(self.frontier) > 0:
             self.ancestors_stack = [self.frontier.pop(0)]  # Limpiamos los ancestros, empezamos desde este nodo
 
@@ -31,7 +32,10 @@ class IDDFS:
                 state = self.ancestors_stack[-1]  # Peek last element (LIFO)
                 self.game.set_state(state)
                 if self.game.has_won():
-                    return state
+                    if found_state is None or found_state.moves > state.moves:
+                        found_state = state
+                        self.ancestors_stack.pop()
+                        break
 
                 delete_ancestor = True
                 for direction in Direction:
@@ -41,12 +45,14 @@ class IDDFS:
                         break
                     elif self.game.get_state().moves == max_depth and self.game.has_won():
                         # Chequeamos en el caso limite que la solucion este a max_depth
-                        return self.game.get_state()
+                        if found_state is None or found_state.moves > self.game.get_state():
+                            found_state = self.game.get_state()
 
                 if delete_ancestor:
                     self.ancestors_stack.pop()  # Si todas las direcciones dan estados ya visitados, no tengo nada mas para hacer
 
-
+        if found_state is not None:
+            return found_state
         # if self.game.has_won():
         #     return self.game.get_state()
         # while len(self.frontier) > 0:
@@ -71,12 +77,13 @@ class IDDFS:
         new_state = self.game.get_state()
 
         not_visited = new_state not in self.visited_nodes
-        if not_visited or self.visited_nodes[new_state] > new_state.moves:
+        different_state = False if not_visited else self.visited_nodes[new_state] > new_state.moves
+        if not_visited or different_state:
             self.visited_nodes[new_state] = new_state.moves
             if new_state.moves == max_depth:
                 self.last_frontier.append(new_state)
                 return False  # No podemos seguir expandiendo este nodo
-            elif not_visited:
+            elif not_visited or different_state:
                 # Si no fue visitado, lo pongo como ancestro y vuelvo al loop
                 self.ancestors_stack.append(new_state)
                 return True
