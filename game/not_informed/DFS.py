d6bdd3662f91f09b49cff9a1bca646cf398e7368
nicbritos <nicbritos@hotmail.com>
2021-08-15 21:25:18 -0300

    Fixeado DF

--- game/not_informed/DFS.py
+++ game/not_informed/DFS.py
@@ -6,22 +6,76 @@ class DFS:
     def __init__(self):
         self.game = Game()
         self.game.parse_board()
-        self.visited_nodes = set()
-        self.frontier = [self.game.get_state()]
+        self.visited_nodes = {self.game.get_state()}
+        self.ancestors_stack = [self.game.get_state()]
+
+    # def process(self):
+    #     while len(self.ancestors_stack) > 0:
+    #         state = self.ancestors_stack[-1]  # Peek last element (LIFO)
+    #         self.game.set_state(state)
+    #         if self.game.has_won():
+    #             return state
+    #
+    #         self.game.move(Direction.DOWN)  # Lo muevo en una direccion
+    #         new_state = self.game.get_state()
+    #         if new_state not in self.visited_nodes:
+    #             # Si no fue visitado, lo pongo como ancestro y vuelvo al loop
+    #             self.ancestors_stack.append(new_state)
+    #             self.visited_nodes.add(new_state)
+    #             continue
+    #
+    #         self.game.set_state(state)
+    #         self.game.move(Direction.LEFT)  # Lo muevo en una direccion
+    #         new_state = self.game.get_state()
+    #         if new_state not in self.visited_nodes:
+    #             # Si no fue visitado, lo pongo como ancestro y vuelvo al loop
+    #             self.ancestors_stack.append(new_state)
+    #             self.visited_nodes.add(new_state)
+    #             continue
+    #
+    #         self.game.set_state(state)
+    #         self.game.move(Direction.UP)  # Lo muevo en una direccion
+    #         new_state = self.game.get_state()
+    #         if new_state not in self.visited_nodes:
+    #             # Si no fue visitado, lo pongo como ancestro y vuelvo al loop
+    #             self.ancestors_stack.append(new_state)
+    #             self.visited_nodes.add(new_state)
+    #             continue
+    #
+    #         self.game.set_state(state)
+    #         self.game.move(Direction.RIGHT)  # Lo muevo en una direccion
+    #         new_state = self.game.get_state()
+    #         if new_state not in self.visited_nodes:
+    #             # Si no fue visitado, lo pongo como ancestro y vuelvo al loop
+    #             self.ancestors_stack.append(new_state)
+    #             self.visited_nodes.add(new_state)
+    #             continue
+    #
+    #         self.ancestors_stack.pop()  # Si todas las direcciones dan estados ya visitados, no tengo nada mas para hacer
 
     def process(self):
-        if self.game.has_won():
-            return self.game.get_state()
-        while len(self.frontier) > 0:
-            state = self.frontier.pop()
+        while len(self.ancestors_stack) > 0:
+            state = self.ancestors_stack[-1]  # Peek last element (LIFO)
+            self.game.set_state(state)
+            if self.game.has_won():
+                return state
 
+            delete_ancestor = True
             for direction in Direction:
                 self.game.set_state(state)
-                self.game.move(direction)
-                if self.game.has_won():
-                    return self.game.get_state()
+                if self._process_direction(direction):
+                    delete_ancestor = False
+                    break
 
-                new_state = self.game.get_state()
-                if new_state not in self.visited_nodes:
-                    self.frontier.append(new_state)
-                    self.visited_nodes.add(new_state)
+            if delete_ancestor:
+                self.ancestors_stack.pop()  # Si todas las direcciones dan estados ya visitados, no tengo nada mas para hacer
+
+    def _process_direction(self, direction: Direction) -> bool:
+        self.game.move(direction)  # Lo muevo en una direccion
+        new_state = self.game.get_state()
+        if new_state not in self.visited_nodes:
+            # Si no fue visitado, lo pongo como ancestro y vuelvo al loop
+            self.ancestors_stack.append(new_state)
+            self.visited_nodes.add(new_state)
+            return True
+        return False
