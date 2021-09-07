755c4a54d3271c4866f08d8bb9c710e9554836a7
Agustín Roca <aroca@itba.edu.ar>
2021-09-06 23:10:06 -0300

    Fix elite

--- TP2/src/main/java/geneticAlgorithm/selection/Elite.java
+++ TP2/src/main/java/geneticAlgorithm/selection/Elite.java
@@ -2,15 +2,13 @@ package geneticAlgorithm.selection;
 
 import models.player.Player;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Elite implements SelectionMethod{
     @Override
     public Collection<Player> select(Collection<Player> players, int k, int generation) {
         List<Player> playersList = new ArrayList<>(players);
+        playersList.sort(Comparator.reverseOrder());
         List<Player> ans = new LinkedList<>();
         for (int i = 0; i < playersList.size(); i++) {
             int times = (int) Math.ceil((double) (k - i)/ playersList.size());
