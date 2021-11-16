8f02ac4cd08f3190d72da651517635d857bce372
Nicolas Britos <nicbritos@hotmail.com>
2021-11-15 21:13:32 -0300

    TP5 - add config

---
+++ TP5/config.py
@@ -0,0 +1,27 @@
+import json
+
+
+class Config:
+    def __init__(self, filename):
+        with open(filename) as file:
+            config = json.load(file)
+
+        aux = config['task']
+        self.exercise = aux['ej']
+        self.font_set = aux['fontSet']
+
+        self.epochs = config['epochs']
+
+        self.eta = config['eta']
+        self.beta = config['beta']
+        self.division_layer = config['divisionLayer']
+
+        aux = config['momentum']
+        self.momentum = aux['enabled']
+        self.momentum_factor = aux['factor']
+
+        aux = config['adaptiveLearning']
+        self.adaptive_learning = aux['enabled']
+        self.adaptive_learning_epochs = aux['epochs']
+        self.adaptive_learning_a = aux['a']
+        self.adaptive_learning_b = aux['b']
